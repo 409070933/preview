0fbde9d8ba01865a4a19ba7d42aabf715c68e3c4
longlong <409070933@qq.com>
2020-12-21 11:18:43 +0800

    init

--- src/main/java/com/preview/controller/PdfController.java
+++ src/main/java/com/preview/controller/PdfController.java
@@ -1,5 +1,6 @@
 package com.preview.controller;
 
+import com.preview.utils.HttpClientUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -7,23 +8,45 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.websocket.server.PathParam;
 import java.io.*;
+import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
 @RestController
+@CrossOrigin
 public class PdfController {
 
-    @RequestMapping(value = "pdf",method = RequestMethod.GET)
-    public ModelAndView onlineView(HttpServletRequest request){
-        ModelAndView modelAndView = new ModelAndView("/pdf/index");
-        return modelAndView;
-    }
+    /**
+     * 保存业务系统标注
+     * @param json
+     */
     @RequestMapping(value = "svePdfLabel",method = RequestMethod.POST)
     @ResponseBody
     public void saveLabe(@RequestBody String json){
+        //回调业务系统pdf标注保存接口
+//        HttpClientUtils.doPost("业务系统url",json);
         System.out.println(json);
 
     }
+
+    /**
+     * 查询业务系统标注
+     * @return
+     */
+    @RequestMapping(value = "pdfLabelList",method = RequestMethod.GET)
+    @ResponseBody
+    public String pdfLabelList(){
+        //回调业务系统pdf标注保存接口
+//        String json =  HttpClientUtils.doGet("业务系统url").toJSONString();
+        String jsonString ="[{\"content\":{\"text\":\"直至执\"},\"position\":{\"boundingRect\":{\"x1\":464.333251953125,\"y1\":1076.515625,\"x2\":521.53759765625,\"y2\":1102.515625,\"width\":991.6666666666666,\"height\":1403.3333333333333},\"rects\":[{\"x1\":464.333251953125,\"y1\":1076.515625,\"x2\":521.53759765625,\"y2\":1102.515625,\"width\":991.6666666666666,\"height\":1403.3333333333333}],\"pageNumber\":3},\"comment\":{\"text\":\"asdas \",\"emoji\":\"\"},\"id\":\"12899956780245203\"}]";
+        return jsonString;
+    }
+
+    /**
+     * 下载pdf
+     * @param response
+     * @param filePath
+     */
     @GetMapping("/download")
     @ResponseBody
     public void download(HttpServletResponse response, @RequestParam String filePath){
